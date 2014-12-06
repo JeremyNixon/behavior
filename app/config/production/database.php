3fcad25a091fbd682fbbbefad4711a8a94be98b4
Jeremy Nixon <jnixon2@gmail.com>
2014-12-06 02:01:51 -0500

    add production server updates

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'tunnel.pagodabox.com:3306',
+            'database'  => 'project',
+            'username'  => 'klyoko',
+            'password'  => '3WFPMKdl',
+        ),
+
+    ),
+
+);
